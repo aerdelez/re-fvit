3f5ba3638f0abe959c695021e47ec2762a40e1e4
stipefrkovic <stipe.frkovic@tutanota.com>
2025-01-18 20:02:38 +0100

    fixes

--- FViT-main/baselines/ViT/generate_visualizations.py
+++ FViT-main/baselines/ViT/generate_visualizations.py
@@ -4,6 +4,14 @@ import h5py
 
 import argparse
 
+import os
+import sys
+import inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+parentdir = os.path.dirname(parentdir)
+sys.path.insert(0, parentdir) 
+
 # Import saliency methods and models
 from misc_functions import *
 
